0e9a449952b822e7e632d27e529656b8a5723f11
Saumya Verma <2k22.cse.2213318@gmail.com>
2026-09-23 09:50:17 +0530

    Time: 4 ms (98.07%), Space: 102 MB (76.11%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -5,17 +5,14 @@ class Solution {
             k += a;
         if (k < 0)
             return -1;
-
         int best = -1, i = 0, s = 0, n = A.length;
         for (int j = 0; j < n; j++) {
             s += A[j];
             while (s > k)
                 s -= A[i++];
-
             if (s == k)
                 best = Math.max(best, j - i + 1);
         }
-
         return best < 0 ? -1 : n - best;
     }
 }
